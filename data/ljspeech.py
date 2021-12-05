6d84861678a15b5e105eca840588a88b62f282a7
Amogha T.R <tramogha@gmail.com>
2021-12-05 09:38:18 +0530

    Update ljspeech.py

--- data/ljspeech.py
+++ data/ljspeech.py
@@ -15,7 +15,7 @@ class LJSpeechDataset(torchaudio.datasets.LJSPEECH):
         return self.transforms({'audio' : audio, 'text': norm_text, 'sample_rate': sample_rate})
 
     def get_text(self, n):
-        line = self._walker[n]
+        line = self._flist[n]
         fileid, transcript, normalized_transcript = line
         return self.transforms({'text' : normalized_transcript})['text']
 
